71d62a919ee871b56dfca5d5a5fb40a7e8c63df9
eduviz08 <62672133+eduviz08@users.noreply.github.com>
2022-12-02 23:14:30 -0700

    Add files via upload

--- scrapy_test_2/scrapy_test_2/spiders/worldometer.py
+++ scrapy_test_2/scrapy_test_2/spiders/worldometer.py
@@ -1,13 +1,18 @@
 import scrapy
+from datetime import date
 
 
         
-        
-class worldometerSpider(scrapy.Spider):
-    name = 'worldometer'
+class worldometerYesterdaySpider(scrapy.Spider):
+    name = 'worldometerYesterday'
     allowed_domains = ['worldometers.info']
     start_urls = ['https://www.worldometers.info/coronavirus/']
+    
+    custom_settings = {
+       'FEEDS': {f'worldometerYesterday_{date.today()}.json': {'format': 'jsonlines', 'overwrite': True}}
+       }
 
+    
     def start_requests(self):
         urls = [
             'https://www.worldometers.info/coronavirus/',
@@ -16,24 +21,38 @@ class worldometerSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
             
     def parse(self,response):
-        #for row in response.xpath('//table[@id="main_table_countries_yesterday"]//tbody/tr/td/a[@class="mt_a"]'):
-        #for row in response.xpath('//*[@class="mt_a"]'):
         for row in response.xpath('//*[@id="main_table_countries_yesterday"]//tbody/tr'):
             yield{
                 'Country' : row.xpath('td[2]//text()').extract(),
                 'Cumulative Deaths' : row.xpath('td[5]//text()').extract(),
                 'New Deaths' : row.xpath('td[6]//text()').extract(),
                 }
-        
 
 
-        
-      
-        
-        
-        
-        
-        
+class worldometer2DaysagoSpider(scrapy.Spider):    
+    name = 'worldometer2Daysago'
+    allowed_domains = ['worldometers.info']
+    start_urls = ['https://www.worldometers.info/coronavirus/']
+    
+    custom_settings = {
+       'FEEDS': {f'worldometer2DaysAgo_{date.today()}.json': {'format': 'jsonlines', 'overwrite': True}}
+       }
+    
+    def start_requests(self):
+        urls = [
+            'https://www.worldometers.info/coronavirus/',
+            ]        
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+            
+    def parse(self,response):
+        for row in response.xpath('//*[@id="main_table_countries_yesterday2"]//tbody/tr'):
+            yield{
+                'Country' : row.xpath('td[2]//text()').extract(),
+                'Cumulative Deaths' : row.xpath('td[5]//text()').extract(),
+                'New Deaths' : row.xpath('td[6]//text()').extract(),
+                }
+             
         
         
         
